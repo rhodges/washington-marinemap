8e9e9b69d26a934fe8e56ab6e0d6611d243fd88e
sfletche <sfletche@gmail.com>
2012-01-17 17:37:06 -0800

    adding fixture for overwater structures model used in smp reports

--- wmm/smp/beach_erosion.py
+++ wmm/smp/beach_erosion.py
@@ -26,10 +26,15 @@ def run_beach_erosion_analysis(smp_obj, type='beach_erosion'):
     area = smp_obj.geometry_final.area
     min_slope, max_slope, avg_slope = get_slope(smp_obj)
     structure_tuples = get_structures(smp_obj)
+    shoremod_perc = get_shoremod_avg(smp_obj)
+    exposed, very_exposed = get_exposure_percs(smp_obj)
+    seagrass, saltmarsh, surfgrass    = get_vegetation_percs(smp_obj)
     #compile context
     context = { 'smp_obj': smp_obj, 'default_value': default_value, 'area': area, 'area_units': settings.DISPLAY_AREA_UNITS,
                 'min_slope': min_slope, 'max_slope': max_slope, 'avg_slope': avg_slope,
-                'structure_tuples': structure_tuples }
+                'structure_tuples': structure_tuples, 'shoremod_perc': shoremod_perc, 
+                'exposed': exposed, 'very_exposed': very_exposed,
+                'seagrass': seagrass, 'saltmarsh': saltmarsh, 'surfgrass': surfgrass }
     return context
     
 def get_slope(smp):
@@ -60,23 +65,51 @@ def get_structures(smp):
     #create_report_cache(bioregion, dict(structures=structure_tuples))
     return structure_tuples    
     
-'''   
-def get_watersheds(bioregion):
-    if report_cache_exists(bioregion, 'watersheds'):
-        watershed_tuples = get_report_cache(bioregion, 'watersheds')
-        return watershed_tuples
-    else:
-        watersheds = Watersheds.objects.filter(geometry__bboverlaps=bioregion.output_geom)
-        watershed_tuples = [(watershed.geometry.intersection(bioregion.output_geom).area, watershed.maj_name) for watershed in watersheds if watershed.geometry.intersects(bioregion.output_geom)]
-        watershed_dict = {}
-        for area,name in watershed_tuples:
-            if name in watershed_dict.keys():
-                watershed_dict[name] += area
-            else:
-                watershed_dict[name] = area
-        watershed_tuples = [(name, convert_float_to_area_display_units(area)) for name,area in watershed_dict.items()]
-        watershed_tuples.sort()
-        watershed_tuples = [(watershed_tuple[0], watershed_tuple[1], convert_sq_km_to_sq_mi(watershed_tuple[1])) for watershed_tuple in watershed_tuples]
-        create_report_cache(bioregion, dict(watersheds=watershed_tuples))
-        return watershed_tuples    
-'''    
+def get_shoremod_avg(smp):
+    shoremod_geom = RasterDataset.objects.get(name='shoremod')
+    shoremod_stats = zonal_stats(smp.geometry_final, shoremod_geom)
+    avg_modification = shoremod_stats.avg / 100
+    return avg_modification
+    
+def get_exposure_percs(smp):
+    exposure_geom = RasterDataset.objects.get(name='exposure')
+    exposure_stats = zonal_stats(smp.geometry_final, exposure_geom)
+    if exposure_stats.pixels:
+        total_pixels = 0.0
+        categories = exposure_stats.categories.all()
+        exposure_dict = {}
+        for cat in categories:
+            if cat.category in [1,2,3,4,5]:
+                exposure_dict[cat.category] = cat.count
+                total_pixels += cat.count
+        exposed = []
+        very_exposed = []
+        if 1 in exposure_dict.keys():
+            exposed = [exposure_dict[1]/total_pixels]
+        if 4 in exposure_dict.keys():
+            very_exposed = [exposure_dict[4]/total_pixels]
+    return exposed, very_exposed
+    
+def get_vegetation_percs(smp): 
+    veg_geom = RasterDataset.objects.get(name='vegetation')
+    veg_stats = zonal_stats(smp.geometry_final, veg_geom)
+    if veg_stats.pixels:
+        total_pixels = 0.0
+        categories = veg_stats.categories.all()
+        veg_dict = {}
+        for cat in categories:
+            if cat.category in [1,2,3,4,5,6,7,8,9,10]:
+                veg_dict[cat.category] = cat.count
+                total_pixels += cat.count
+        seagrass = []
+        saltmarsh = []
+        surfgrass = []
+        if 7 in veg_dict.keys():
+            seagrass = [veg_dict[7]/total_pixels]
+        if 6 in veg_dict.keys():
+            saltmarsh = [veg_dict[6]/total_pixels]
+        if 9 in veg_dict.keys():
+            surfgrass = [veg_dict[9]/total_pixels]
+    return seagrass, saltmarsh, surfgrass   
+    
+  
