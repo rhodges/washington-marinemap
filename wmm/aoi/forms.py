d41116bcc1ed2f72441c372e7690091d6c6926a9
sfletche <sfletche@gmail.com>
2012-02-02 14:32:31 -0800

    removing unnecessary form fields from aoi form

--- wmm/aoi/forms.py
+++ wmm/aoi/forms.py
@@ -7,6 +7,9 @@ from models import *
 class AoiForm(SpatialFeatureForm):
     class Meta(SpatialFeatureForm.Meta):
         model = AOI
+        exclude = list(SpatialFeatureForm.Meta.exclude)
+        exclude.append('geometry_hash')
+        exclude.append('benthic_score')
 
 class LoiForm(SpatialFeatureForm):
     class Meta(SpatialFeatureForm.Meta):
