29c06882ecf33c97b8c55fda5b9b283282058153
sfletche <sfletche@gmail.com>
2012-02-16 18:04:55 -0800

    exluding fields from aoi form template

--- wmm/aoi/forms.py
+++ wmm/aoi/forms.py
@@ -8,6 +8,10 @@ class AoiForm(SpatialFeatureForm):
     class Meta(SpatialFeatureForm.Meta):
         model = AOI
         exclude = list(SpatialFeatureForm.Meta.exclude)
+        #not sure why I get a form error when I include the following within a single exclude.append...
         exclude.append('geometry_hash')
         exclude.append('conservation_score')
+        exclude.append('tidalenergy_score')
+        exclude.append('waveenergy_score')
+        exclude.append('windenergy_score')
 
