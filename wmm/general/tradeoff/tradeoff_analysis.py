477160492faaf07666bdf4205f5b26633a4c0396
sfletche <sfletche@gmail.com>
2012-03-09 15:44:52 -0800

    adding the ability to select shapes for tradeoff analysis

--- wmm/general/tradeoff/tradeoff_analysis.py
+++ wmm/general/tradeoff/tradeoff_analysis.py
@@ -18,19 +18,19 @@ objective_list = [ "Conservation", "Tidal Energy", "Wind Energy", "Wave Energy" 
     
 '''
 '''
-def display_tradeoff_analysis(request, folder, x_axis, y_axis, template='folder/reports/tradeoff_report.html'):
-    context = run_tradeoff_analysis(folder, x_axis, y_axis)
+def display_tradeoff_analysis(request, folder, x_axis, y_axis, smp_ids, aoi_ids, template='folder/reports/tradeoff_report.html'):
+    context = run_tradeoff_analysis(folder, x_axis, y_axis, smp_ids, aoi_ids)
     return render_to_response(template, RequestContext(request, context)) 
 
 '''
 Run the analysis, create the cache, and return the results as a context dictionary so they may be rendered with template
 '''    
-def run_tradeoff_analysis(folder, x_axis, y_axis): 
+def run_tradeoff_analysis(folder, x_axis, y_axis, smp_ids, aoi_ids): 
     x_label = '%s Score' %x_axis
     y_label = '%s Score' %y_axis
     x_type = get_type(x_axis)
     y_type = get_type(y_axis)
-    sites, site_names, site_colors = get_chart_attributes(folder, x_type, y_type)
+    sites, site_names, site_colors = get_chart_attributes(folder, x_type, y_type, smp_ids, aoi_ids)
     legend_location = determine_legend_location(sites)
     #conservation_colors = [ "#4bb2c5", "#4bb2c5", "#4bb2c5", "#4bb2c5" ]
     #from general.utils import kmlcolor_to_htmlcolor
@@ -41,28 +41,29 @@ def run_tradeoff_analysis(folder, x_axis, y_axis):
     
 '''
 '''    
-def get_chart_attributes(folder, x_axis, y_axis):
-    aoi_attributes, aoi_names, aoi_colors, color_index = get_chart_attributes_from_feature_set(folder.aoi_set, x_axis, y_axis)
-    smp_attributes, smp_names, smp_colors, color_index = get_chart_attributes_from_feature_set(folder.smp_set, x_axis, y_axis, color_index=color_index)
+def get_chart_attributes(folder, x_axis, y_axis, smp_ids, aoi_ids):
+    aoi_attributes, aoi_names, aoi_colors, color_index = get_chart_attributes_from_feature_set(folder.aoi_set, x_axis, y_axis, aoi_ids)
+    smp_attributes, smp_names, smp_colors, color_index = get_chart_attributes_from_feature_set(folder.smp_set, x_axis, y_axis, smp_ids, color_index=color_index)
     attributes = aoi_attributes + smp_attributes
     names = aoi_names + smp_names
     colors = aoi_colors + smp_colors
     return attributes, names, colors
     
-def get_chart_attributes_from_feature_set(feature_set, x_axis, y_axis, color_index=0):
+def get_chart_attributes_from_feature_set(feature_set, x_axis, y_axis, requested_ids, color_index=0):
     attributes = []
     names = []
     colors = []
     for feature in feature_set:
-        x_value = get_score(feature, x_axis)
-        y_value = get_score(feature, y_axis)
-        nlog_size = log(sq_meters_to_sq_miles(feature.geometry_final.area))
-        name = str(feature.name)
-        names.append(name)
-        colors.append(series_colors[color_index%10])
-        attribute_list = [x_value, y_value, nlog_size, name]
-        attributes.append(attribute_list)
-        color_index += 1
+        if feature.id in requested_ids:
+            x_value = get_score(feature, x_axis)
+            y_value = get_score(feature, y_axis)
+            nlog_size = log(sq_meters_to_sq_miles(feature.geometry_final.area))
+            name = str(feature.name)
+            names.append(name)
+            colors.append(series_colors[color_index%10])
+            attribute_list = [x_value, y_value, nlog_size, name]
+            attributes.append(attribute_list)
+            color_index += 1
     return attributes, names, colors, color_index
     
      
@@ -110,14 +111,14 @@ def get_quadrant(x,y):
     
 '''
 '''
-def display_tradeoff_table(request, folder, template='folder/reports/tradeoff_table.html'):
-    context = get_tradeoff_table_context(folder)   
+def display_tradeoff_table(request, folder, smp_ids, aoi_ids, template='folder/reports/tradeoff_table.html'):  
+    context = get_tradeoff_table_context(folder, smp_ids, aoi_ids)   
     return render_to_response(template, RequestContext(request, context))
     
 '''
 '''
-def get_tradeoff_table_context(folder):    
-    sites = get_table_attributes(folder)
+def get_tradeoff_table_context(folder, smp_ids, aoi_ids): 
+    sites = get_table_attributes(folder, smp_ids, aoi_ids)
     context = {'default_value': default_value, 'sites': sites, 'objective_list': objective_list, 'threshold': THRESHOLD}
     return context
 
@@ -140,24 +141,25 @@ def get_type(objective):
 
 '''
 '''
-def get_table_attributes(folder):
-    aoi_sites = get_table_attributes_from_feature_set(folder.aoi_set)
-    smp_sites = get_table_attributes_from_feature_set(folder.smp_set)
+def get_table_attributes(folder, smp_ids, aoi_ids):
+    aoi_sites = get_table_attributes_from_feature_set(folder.aoi_set, aoi_ids)
+    smp_sites = get_table_attributes_from_feature_set(folder.smp_set, smp_ids)
     sites = aoi_sites + smp_sites
     return sites
     
 '''
 '''    
-def get_table_attributes_from_feature_set(feature_set): 
+def get_table_attributes_from_feature_set(feature_set, requested_ids): 
     sites = [] 
     for feature in feature_set:
-        site = {}
-        site['name'] = str(feature.name)
-        site['color'] = series_colors[0]
-        scores = get_scores(feature)
-        site['scores'] = scores 
-        site['conflict'] = has_conflict(scores)
-        sites.append(site)      
+        if feature.id in requested_ids:
+            site = {}
+            site['name'] = str(feature.name)
+            site['color'] = series_colors[0]
+            scores = get_scores(feature)
+            site['scores'] = scores 
+            site['conflict'] = has_conflict(scores)
+            sites.append(site)      
     return sites
 
 '''
--- wmm/general/views.py
+++ wmm/general/views.py
@@ -11,6 +11,8 @@ def tradeoff_analysis(request):
         x_axis = request.POST.getlist('selected_x')[0]
         y_axis = request.POST.getlist('selected_y')[0]
         folder_id = getlist(request, 'folder_id')[0]
+        smp_ids = getlist(request, 'smps[]')
+        aoi_ids = getlist(request, 'aois[]')
         
         folder_obj = get_object_or_404(Folder, pk=folder_id)
         #check permissions
@@ -19,14 +21,15 @@ def tradeoff_analysis(request):
             return response
         
         from tradeoff.tradeoff_analysis import display_tradeoff_analysis
-        return display_tradeoff_analysis(request, folder_obj, x_axis, y_axis)
+        return display_tradeoff_analysis(request, folder_obj, x_axis, y_axis, smp_ids, aoi_ids)
     
 '''
 '''
 def tradeoff_table(request):
     if (request.POST):
         folder_id = getlist(request, 'folder_id')[0]
-        
+        smp_ids = getlist(request, 'smps[]')
+        aoi_ids = getlist(request, 'aois[]')
         folder_obj = get_object_or_404(Folder, pk=folder_id)
         #check permissions
         viewable, response = folder_obj.is_viewable(request.user)
@@ -34,7 +37,7 @@ def tradeoff_table(request):
             return response
             
         from tradeoff.tradeoff_analysis import display_tradeoff_table
-        return display_tradeoff_table(request, folder_obj)        
+        return display_tradeoff_table(request, folder_obj, smp_ids, aoi_ids)        
     
 def getlist(request, param_string):
     string_list = request.POST.getlist(param_string)
