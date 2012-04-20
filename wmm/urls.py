d4a6a71f2b7c5bab7f6289e2c74c4d851b443a33
sfletche <sfletche@gmail.com>
2012-04-20 12:03:45 -0700

    adding disclaimer

--- wmm/urls.py
+++ wmm/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic.base import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,5 +11,6 @@ urlpatterns = patterns('',
     (r'^general/', include('general.urls')),
     (r'^scenario/', include('scenario.urls')),
     (r'^smp/', include('smp.urls')),
-    (r'^aoi/', include('aoi.urls'))
+    (r'^aoi/', include('aoi.urls')),
+    url(r'^disclaimer/$', TemplateView.as_view(template_name='disclaimer/show.html')),
 )
